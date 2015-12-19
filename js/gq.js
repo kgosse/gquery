c640289d2d016726d63c0dbcda59b7ba67fe475f
Kevin Gosse <gossekevin1@gmail.com>
2015-12-19 20:56:45 +0100

    implement the module design pattern

---
+++ js/gq.js
@@ -0,0 +1,33 @@
+(function(scope){
+    var version = 1.0001;
+
+    var gQ = function(selector, context){
+
+    };
+    gQ.loadJS = function(){
+
+    };
+    gQ.version = function(){
+        return version;
+    };
+
+    if(!scope.gQ)
+        scope.gQ = gQ;
+    else
+    {
+
+    }
+}(window));
+
+var com = com || {};
+com.o2GEEK = com.o2GEEK || {};
+
+com.o2GEEK.gQ = function(selector, context){
+
+};
+
+if(!com.o2GEEK.gQ) {
+    com.o2GEEK.gQ.loadJS = function () {
+
+    }
+}
