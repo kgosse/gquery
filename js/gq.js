5324d06f7fc225b801e676793d6b66842c899ac7
Kevin Gosse <gossekevin1@gmail.com>
2015-12-19 21:44:28 +0100

    make the lib work

--- js/gq.js
+++ js/gq.js
@@ -1,33 +1,75 @@
 (function(scope){
     var version = 1.0001;
+    var doc = scope.document;
+    var q;
 
     var gQ = function(selector, context){
-
+        return q(selector);
     };
-    gQ.loadJS = function(){
+    gQ.loadJS = function(path, callback){
+        var js = doc.createElement('script');
+        js.src = path;
+        js.type = 'text/javascript';
+        js.onload = function(){
+            callback();
+            this.onload = this.onreadystatechange = null;
+        };
 
+        js.onreadystatechange = function(){
+            if(this.readState == 'complete'){
+                this.onload();
+            }
+        };
+        doc.getElementsByTagName('head')[0].appendChild(js);
     };
+    gQ.start = function(){};
     gQ.version = function(){
         return version;
     };
+
+    gQ.ready = function(fun){
+        var last = scope.onload;
+        var isReady = false;
+
+        if(doc.addEventListener){
+            doc.addEventListener('DOMContentLoaded', function(){
+                console.log("DOM is loaded");
+                isReady = true;
+                fun();
+            });
+        }
+
+        scope.onload = function(){
+            if (last) last();
+
+            if(isReady) fun();
+        }
+    };
+
+
+    gQ.ready(function(){
+        if(doc.querySelectorAll && doc.querySelectorAll("body:first-of-type")){
+            q = function(parm){
+                return doc.querySelectorAll(parm);
+            };
+            gQ.start();
+        }
+        else{
+            gQ.loadJS('js/sizzle.min.js', function(){
+                q = Sizzle;
+                gQ.start();
+            });
+        }
+    });
 
     if(!scope.gQ)
         scope.gQ = gQ;
     else
     {
-
+        if(scope.gQ.version)
+            scope.gQ = scope.gQ.version() > version ? scope.gQ : gQ;
+        else
+            throw new Error("gQ is not defined!");
     }
+
 }(window));
-
-var com = com || {};
-com.o2GEEK = com.o2GEEK || {};
-
-com.o2GEEK.gQ = function(selector, context){
-
-};
-
-if(!com.o2GEEK.gQ) {
-    com.o2GEEK.gQ.loadJS = function () {
-
-    }
-}
--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
-var q;
 
-window.onload = function(){
-    console.log("window is loaded.");
+gQ.start = function(){
+    console.log(gQ('.article'));
 };
-
-addOnReady(function(){
-    console.log("2 window is loaded.");
-});
-
-addOnReady(function(){
-    console.log("3 window is loaded.");
-
-    if(document.querySelectorAll && document.querySelectorAll("body")[0])
-        q = function(parm){
-            return document.querySelectorAll(parm);
-        };
-});
-
