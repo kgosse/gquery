f5a5255c79086615d5024dae8b3c90c670cc1b26
Kevin Gosse <gossekevin1@gmail.com>
2015-12-30 21:55:05 +0100

    create an event dispatcher

--- js/gq.js
+++ js/gq.js
@@ -205,6 +205,37 @@
         }
     })();
 
+    function EventDispatcher(o){
+        var list = {};
+
+        o.addEvent = function(type, listener){
+            if (!list[type])
+                list[type] = [];
+            if (list[type].indexOf(listener) == -1)
+                list[type].push(listener);
+        };
+
+        o.dispatchEvent = function(e){
+            var a = list[e.type];
+            if (a){
+                if (!e.target)
+                    e.target = this;
+                for (var index in a){
+                    a[index].call(e.target, e);
+                }
+            }
+        };
+    }
+
+    var o = {};
+    EventDispatcher(o);
+
+    o.addEvent('tick', function(e){
+        console.log("a tick just happend", e.target, e.type);
+        console.log(this == o, this, o);
+    });
+
+    o.dispatchEvent({type: 'tick', target: o});
 
     if(!scope.gQ)
         scope.gQ = gQ;
